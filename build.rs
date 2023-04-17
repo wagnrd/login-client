4f12cb7c9f456ea0dc23b084291cfc77c65cfb2c
Denis Wagner <denis.wagner@check24.de>
2023-04-18 00:02:50 +0200

    adds loading and launching panels

--- build.rs
+++ build.rs
@@ -1,3 +1,6 @@
 fn main() {
-    slint_build::compile("ui/main_window.slint").unwrap();
+    let config = slint_build::CompilerConfiguration::new()
+        .embed_resources(slint_build::EmbedResourcesKind::EmbedForSoftwareRenderer);
+    slint_build::compile_with_config("ui/main_window.slint", config).unwrap();
+    slint_build::print_rustc_flags().unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-use slint::Model;
+mod auth0_login;
 
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
     let main_window = MainWindow::new().unwrap();
-    main_window.on_on_login_clicked(|login_data| {
-        println!("username: {} - passowrd: {}", login_data.username, login_data.password);
+    let main_window_clone = main_window.clone_strong();
+    main_window.on_login_clicked(move |login_data| {
+        println!("username: {} - password: {}", login_data.username, login_data.password);
+        main_window_clone.set_error("Wrong credentials".into());
     });
     main_window.run()
 }
