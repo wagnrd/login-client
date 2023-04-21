7d615ea79d72ac9cf522ec89fe69aa8bdb402514
wagnrd <denis@wagnrd.de>
2023-04-21 11:04:39 +0200

    removes slint interface from test_app

--- test_app/src/main.rs
+++ test_app/src/main.rs
@@ -1,21 +1,53 @@
-use slint::slint;
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
 
-slint! {
-    import { VerticalBox, HorizontalBox } from "std-widgets.slint";
+    let mut id_token: String = String::new();
+    let mut access_token: String = String::new();
+    let mut is_invalid = true;
 
-    export component MainWindow inherits Window {
-        title: "Test App";
-        width: 200px;
-        height: 200px;
+    if args.len() >= 5 {
+        is_invalid = false;
 
-        Text {
-            text: "Test App";
+        match get_token_type(&args[1]) {
+            TokenType::IdToken => id_token = args[2].clone(),
+            TokenType::AccessToken => access_token = args[2].clone(),
+            TokenType::Invalid => is_invalid = true,
         }
+
+        match get_token_type(&args[3]) {
+            TokenType::IdToken => id_token = args[4].clone(),
+            TokenType::AccessToken => access_token = args[4].clone(),
+            TokenType::Invalid => is_invalid = true,
+        }
+    }
+
+    match is_invalid {
+        false => println!("id_token: {}\naccess_token: {}", id_token, access_token),
+        true => println!("Invalid args: {}", args_to_string()),
     }
 }
 
-fn main() -> Result<(), slint::PlatformError> {
-    MainWindow::new()
-        .unwrap()
-        .run()
+enum TokenType {
+    IdToken,
+    AccessToken,
+    Invalid,
+}
+
+fn get_token_type(option: &String) -> TokenType {
+    match option.as_str() {
+        "--id_token" => TokenType::IdToken,
+        "--access_token" => TokenType::AccessToken,
+        _ => TokenType::Invalid
+    }
+}
+
+fn args_to_string() -> String {
+    let args: Vec<String> = std::env::args().collect();
+    let mut result = String::new();
+
+    for arg in args {
+        result += format!("{arg} ").as_str();
+    }
+
+    result
 }
